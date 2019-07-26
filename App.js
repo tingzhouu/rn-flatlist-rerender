f279aa539fbac893f2a0dee376e25efa5406cb5d
Ting Zhou <limtingzhou@gmail.com>
2019-07-26 12:28:18 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+
+import FlatListRerender from './src/FlatListRerender';
+
+const App = () => {
+  return (
+    <FlatListRerender />
+  );
+};
+
+export default App;
